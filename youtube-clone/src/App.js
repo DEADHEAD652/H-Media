b1a6dc2929f1adaf38f30543ebfd7574e3ec02b7
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-04-06 00:19:07 +0500

    initial commit

---
+++ youtube-clone/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import {Box} from '@mui/material'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Box></Box>
+    
+    </BrowserRouter>
+  )
+}
+
+export default App
