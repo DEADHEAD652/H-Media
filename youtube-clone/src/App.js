5e41187fd2c2a2fe718cfaddf9601d3ce9d7b7c5
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-04-06 16:03:42 +0500

    navbar

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
-import {Box} from '@mui/material'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  Feed,
+  VedioDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./Components";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Box></Box>
-    
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/vedio/:id" element={<VedioDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
