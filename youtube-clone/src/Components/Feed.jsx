cdd62fbf78b4b7e9a4b99b64d6c37db1cba788b5
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-04-06 19:32:31 +0500

    sidebar

--- youtube-clone/src/Components/Feed.jsx
+++ youtube-clone/src/Components/Feed.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { borderRight } from "@mui/system";
+import SideBar from "./SideBar";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const date = new Date();
+  const year  = date.getFullYear();
+  return (<Stack  sx={{flexDirection :{sx:'column' , md: "row"}}} >
 
-export default Feed
+<Box sx={{height: {sx:'auto',md :'92vh'}, borderRight:'1px solid #3d3d3d' , px:{ sx:0 , md:'2'}}}  >
+
+<SideBar />
+<Typography className="copyright " variant="body2" sx={{mt:1.5,color:'#fff'}}>
+
+&copy; Copyright H Tube {year}
+
+</Typography>
+
+
+</Box>
+
+
+
+  </Stack>)
+};
+
+export default Feed;
