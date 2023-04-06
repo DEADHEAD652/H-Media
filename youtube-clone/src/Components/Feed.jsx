10b87e8e74a3cb3a39fb2b5a3f650d3f417180d3
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-04-06 22:53:39 +0500

    video feed

--- youtube-clone/src/Components/Feed.jsx
+++ youtube-clone/src/Components/Feed.jsx
@@ -1,28 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import { borderRight } from "@mui/system";
-import SideBar from "./SideBar";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+import { SideBar,Videos} from './'
 
 const Feed = () => {
+
+  const [selectedCategory,setSelectedCategory] = useState('New')
+  const [videos,setVideos] = useState([] )
+  useEffect(() =>{
+fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => setVideos(data.items))
+
+
+  },[selectedCategory])
+
+
   const date = new Date();
-  const year  = date.getFullYear();
-  return (<Stack  sx={{flexDirection :{sx:'column' , md: "row"}}} >
+  const year = date.getFullYear();
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{ 
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: "2" },
+        }}
+      >
+        <SideBar
+        
+        selectedCategory={selectedCategory}
+setSelectedCategory={setSelectedCategory} 
 
-<Box sx={{height: {sx:'auto',md :'92vh'}, borderRight:'1px solid #3d3d3d' , px:{ sx:0 , md:'2'}}}  >
-
-<SideBar />
-<Typography className="copyright " variant="body2" sx={{mt:1.5,color:'#fff'}}>
-
-&copy; Copyright H Tube {year}
-
-</Typography>
-
-
-</Box>
-
-
-
-  </Stack>)
+/>
+        <Typography
+          className="copyright "
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          &copy; Copyright H Tube {year}
+        </Typography>
+      </Box>
+      <Box p={2} sx={{overflowY:'auto', height:'90vh' ,flex:'2'}}>
+        <Typography variant='h4' fontWeight='bold'  mb={2} sx={{color:"white"}}>
+          {selectedCategory} <span style={{ color: "#F31503" }}> videos</span>
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
--- youtube-clone/src/Components/SideBar.jsx
+++ youtube-clone/src/Components/SideBar.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import { Stack } from "@mui/material";
 import { categories } from "../utils/constants";
 
-const Sidebar = () => {
-  const selectedCategory = "New";
+const Sidebar = ({selectedCategory,setSelectedCategory}) => {
   return (
     <Stack
       direction="row"
       sx={{
-        overflow: "auto",
+        overflowY: "auto",
         height: { sx: "auto", md: "95%" },
         flexDirection: { md: "column" },
       }}
+      
     >
       {categories.map((category) => (
         <button
           className="category-btn"
+          onClick={() => setSelectedCategory(category.name)}
           style={{
             backgroundColor: category.name === selectedCategory && "#FC1503",
             color: "white",
